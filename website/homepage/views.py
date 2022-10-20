4ac95aa7deab9de68187c4ad691d0c6f201a8631
knreano <reanokeanu@gmail.com>
2022-10-20 15:16:56 +0800

    added bootstrap

--- website/homepage/views.py
+++ website/homepage/views.py
@@ -1,9 +1,29 @@
+from turtle import pos
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Dummy posts
+posts = [
+    {
+        'author': 'Keanu Reano',
+        'title': 'Blog Post 1',
+        'content': 'Labore excepteur ullamco amet ut aliqua. Commodo aliqua tempor laborum sunt eiusmod.',
+        'date_posted': 'November 17, 2022'
+    },
+    {
+        'author': 'Keanu Reano',
+        'title': 'Blog Post 2',
+        'content': 'Nulla est dolore eu eiusmod laborum culpa deserunt laboris Lorem.',
+        'date_posted': 'November 19, 2022'
+    }
+]
+
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'homepage/index.html', context)
 
 def about(request):
-    return render(request, 'homepage/about.html')
+    return render(request, 'homepage/about.html', {'title': 'About'})
--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
     'livereload',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage',
 ]
 
 MIDDLEWARE = [
